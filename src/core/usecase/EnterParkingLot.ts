08d57666bec11d91e1f07799e430e7ca65837ba9
matheus <mathparro@gmail.com>
2023-07-18 21:30:57 -0300

    understanding adapters

--- src/core/usecase/EnterParkingLot.ts
+++ src/core/usecase/EnterParkingLot.ts
@@ -1,4 +1,4 @@
-import ParkingLot from "../entity/ParkingLot";
+import ParkedCar from "../entity/ParkedCar";
 import ParkingLotRepository from "../repository/ParkingLotRepository";
 
 export default class EnterParkingLot {
@@ -6,8 +6,18 @@ export default class EnterParkingLot {
   constructor(parkingLotRepository: ParkingLotRepository) {
     this.parkingLotRepository = parkingLotRepository;
   }
-  async execute(code: string) {
+  async execute(code: string, plate: string, date: Date) {
+    const parkedCar = new ParkedCar(code, plate, date);
     const parkingLot = await this.parkingLotRepository.getParkingLot(code);
+    if (!parkingLot.isOpen(parkedCar.date))
+      throw new Error("the park lot is closed");
+
+    if (parkingLot.isFull()) throw new Error("the park lot is full");
+    await this.parkingLotRepository.saveParkedCar(
+      parkedCar.code,
+      parkedCar.plate,
+      parkedCar.date
+    );
     return parkingLot;
   }
 }
--- src/infra/Repository/ParkingLotRepositoryMemory.ts
+++ src/infra/Repository/ParkingLotRepositoryMemory.ts
@@ -1,3 +1,4 @@
+import ParkingLotAdapter from "../../adapter/ParkingLotAdapter";
 import ParkingLot from "../../core/entity/ParkingLot";
 import ParkingLotRepository from "../../core/repository/ParkingLotRepository";
 
@@ -5,7 +6,34 @@ export default class ParkingLotRepositoryMemory
   implements ParkingLotRepository
 {
   constructor() {}
+
+  parkingLots = [
+    {
+      code: "shopping",
+      capacity: 5,
+      openHour: 8,
+      closeHour: 22,
+      occupiedSpaces: 0,
+    },
+  ];
+  parkedCars = [];
   getParkingLot(code: string): Promise<ParkingLot> {
-    return Promise.resolve(new ParkingLot(code, 5, 8, 22));
+    const parkingLotData = this.parkingLots.find(
+      (itemParkingLots) => itemParkingLots.code === code
+    );
+    const occupiedSpaces = this.parkedCars.length;
+    const parkingLot = ParkingLotAdapter.create(
+      parkingLotData.code,
+      parkingLotData.capacity,
+      parkingLotData.openHour,
+      parkingLotData.closeHour,
+      occupiedSpaces
+    );
+    parkingLot.occupiedSpaces = this.parkedCars.length;
+    return Promise.resolve(parkingLot);
+  }
+
+  saveParkedCar(code: string, plate: string, date: Date): void {
+    this.parkedCars.push({ code, plate, date });
   }
 }
